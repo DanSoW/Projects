3dd1b3f3a76c0affa61cab60d36a57cb2579f803
Daniil <swdaniel@yandex.ru>
2021-02-01 14:28:56 +0800

    Init

---
+++ app/src/main/java/com/example/androidcanvas/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.androidcanvas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(new Draw(this));
+    }
+}
